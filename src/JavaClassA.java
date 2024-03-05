85ff3fe248b5161c975e1879493a77930a6e2e07
sharvariprasade <sharvariprasade0805@gmail.com>
2024-03-05 15:03:03 +0530

    This is my second commit

--- src/JavaClassA.java
+++ src/JavaClassA.java
@@ -3,7 +3,7 @@ public class JavaClassA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Demo Sharvari");
+		System.out.println("New Demo Sharvari");
 	}
 
 }
